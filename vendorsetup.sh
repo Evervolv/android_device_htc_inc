1270f8cad8f7f0f2fdb94924944bcde58fb9d3eb
andrew.boren <andrew.boren@gmail.com>
2011-03-01 21:12:27 -0700

    inc: Remove non-evervolv build options.
    Change-Id: I1d9c2a4d8ba8668f05a43e5bee2a5482dc7aa3a4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_inc-userdebug
+#We're only going to be building Evervolv, so comment this out for now
+#add_lunch_combo htc_inc-userdebug
+#add_lunch_combo htc_inc-eng
