8ccf6f4b67c6a8e3c5408000b9b31b4b5e971bfb
Paul Mongold <pmongold@gmail.com>
2012-01-10 22:36:07 -0500

    inc: bump to p4
    Change-Id: I8c33205142fcd8882c5040ae758a296509e7d1bb

--- ev.mk
+++ ev.mk
@@ -16,7 +16,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRJ22 BUILD_FINGERPRINT=verizon_wwe/inc
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Dives
-PRODUCT_VERSION_DEVICE_SPECIFIC := p3
+PRODUCT_VERSION_DEVICE_SPECIFIC := p4
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Droid Incredible\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
