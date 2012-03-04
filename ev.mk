55c9d227bfcda916248d5cf17277ba414d508a01
andrew.boren <andrew.boren@gmail.com>
2012-03-03 20:17:44 -0700

    inc: Don't override BUILD_ID.
    Change-Id: I9bffa2a8866f2b7ac05272a917bdfa3bdf3ca599

--- ev.mk
+++ ev.mk
@@ -12,7 +12,7 @@ PRODUCT_BRAND := verizon_wwe
 PRODUCT_DEVICE := inc
 PRODUCT_MODEL := Droid Incredible
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRJ22 BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.3.4/GRJ22/185897.2:user/release-keys PRIVATE_BUILD_DESC="4.08.605.2 CL185897 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.3.4/GRJ22/185897.2:user/release-keys PRIVATE_BUILD_DESC="4.08.605.2 CL185897 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Dives
