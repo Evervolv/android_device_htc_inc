b4dd79c47e4eddec338d545b7f0a70b90fb56895
Paul Mongold <pmongold@gmail.com>
2012-07-10 15:49:24 -0400

    liblights - only build for inc & change LOG lines to ALOG

--- liblights/lights.c
+++ liblights/lights.c
@@ -76,7 +76,7 @@ write_int(char const* path, int value)
         return amt == -1 ? -errno : 0;
     } else {
         if (already_warned == 0) {
-            LOGE("write_int failed to open %s\n", path);
+            ALOGE("write_int failed to open %s\n", path);
             already_warned = 1;
         }
         return -errno;
@@ -157,7 +157,7 @@ set_speaker_light_locked(struct light_device_t* dev,
     colorRGB = state->color;
 
 #if 0
-    LOGD("set_speaker_light_locked colorRGB=%08X, onMS=%d, offMS=%d\n",
+    ALOGD("set_speaker_light_locked colorRGB=%08X, onMS=%d, offMS=%d\n",
             colorRGB, onMS, offMS);
 #endif
 
