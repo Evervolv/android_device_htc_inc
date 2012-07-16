8aeb0e6818c5aa5a401dbef78aeffdb489bb5ad0
Paul Mongold <pmongold@gmail.com>
2012-07-16 03:42:17 -0400

    inc: bump to p2
    Change-Id: I9e1f9d4332650bfafa7a9c1a24dceddd10fbd199

--- ev.mk
+++ ev.mk
@@ -16,7 +16,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=verizon_wwe/inc/inc/inc:2.3.4/
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Dives
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1
+PRODUCT_VERSION_DEVICE_SPECIFIC := p2
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv\nVisit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest updates\n------------------------------------------------\n"
 
